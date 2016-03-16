54ef3a1fef20a37d689adf1fc2a5ffdc883d7460
Levin Keller <post@levinkeller.de>
2016-03-16 17:47:35 +0100

    Adding heavy load test.

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var Fixer = require('../index');
-
+var faker = require('faker');
 var fixer = new Fixer();
+var async = require('async');
 
 describe('Basic Tests', function() {
     it('should convert 1 Dollar to Euro', function(done) {
@@ -11,5 +12,16 @@ describe('Basic Tests', function() {
           console.log(result);
            done();
        });
+    });
+});
+
+describe('Heavy load', function() {
+    this.timeout(30000);
+    it('should convert 1 Dollar 1000 times to Euro', function(done) {
+        async.times(1000, function(n, callback) {
+            var date = faker.date.between('2011-03-12','2015-03-12');
+            console.log(date);
+            fixer.convert('USD','EUR',date,1, callback);
+        }, done);
     });
 });
