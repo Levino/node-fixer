79e9bd819a76c97c4817f7b8018d5cda920059c2
Levin Keller <post@levinkeller.de>
2017-04-02 21:07:18 +0200

    Adding support for cache.

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 var Fixer = require('../index')
 var faker = require('faker')
-var fixer = new Fixer()
 var async = require('async')
-
+var path = require('path')
 describe('Basic Tests', function () {
   it('should convert 1 Dollar to Euro', function (done) {
+    var fixer = new Fixer()
     fixer.convert('USD', 'EUR', new Date('2012-03-12'), 1, function (err, result) {
       if (err) {
         return done(err)
@@ -18,6 +18,19 @@ describe('Basic Tests', function () {
 describe('Heavy load', function () {
   this.timeout(30000)
   it('should convert 1 Dollar 1000 times to Euro', function (done) {
+    var fixer = new Fixer()
+    async.times(1000, function (n, callback) {
+      var date = faker.date.between('2011-03-12', '2015-03-12')
+      console.log(date)
+      fixer.convert('USD', 'EUR', date, 1, callback)
+    }, done)
+  })
+})
+
+describe('Test cache', function () {
+  this.timeout(30000)
+  it('should convert 1 Dollar 1000 times to Euro', function (done) {
+    var fixer = new Fixer({cacheDir: path.join(__dirname, '/../cache')})
     async.times(1000, function (n, callback) {
       var date = faker.date.between('2011-03-12', '2015-03-12')
       console.log(date)
